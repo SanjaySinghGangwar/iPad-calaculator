a73fb6966108c0a60b673e768dc7ca77eed2fc5a
sanjay s gangwar <dowqix-qostam-gotTy0>
2022-04-07 13:35:33 +0530

    Initial Commit

---
+++ calaculator/calaculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calaculatorApp.swift
+//  calaculator
+//
+//  Created by HGNIS on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct calaculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
